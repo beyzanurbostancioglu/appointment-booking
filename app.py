81da1c6de743824ef5724051ae383ee4063efec7
beyzanurbostancioglu <153711928+beyzanurbostancioglu@users.noreply.github.com>
2025-06-13 13:58:22 +0300

    Initial project setup

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, redirect
+import sqlite3
+
+app = Flask(__name__)
+
+# Ana sayfa
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
